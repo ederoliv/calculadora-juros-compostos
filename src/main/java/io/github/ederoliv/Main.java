07b1c77a53159acb3531c54fdc8616ffee9bea75
p41nwt55 <ederoliveira152@gmail.com>
2024-02-21 15:31:19 -0300

    First commit

--- src/main/java/io/github/ederoliv/Main.java
+++ src/main/java/io/github/ederoliv/Main.java
@@ -2,6 +2,19 @@ package io.github.ederoliv;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Calculadora");
+
+        /*
+        A = P (1 + i) ^ t
+
+    Para entender melhor a fórmula segue a explicação de cada variável:
+
+    A = valor final, ou seja, o resultado que você terá;
+    P = valor inicial depositado;
+    i = taxa de juros;
+    t = tempo do investimento.
+
+         */
     }
 }
