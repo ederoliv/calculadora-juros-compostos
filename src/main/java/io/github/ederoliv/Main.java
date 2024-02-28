1e2f8d3b8c10d618290393305e9b0e7a95e2ca90
ederoliv <ederoliveira152@gmail.com>
2024-02-27 22:15:12 -0300

    Add gitignore

--- src/main/java/io/github/ederoliv/Main.java
+++ src/main/java/io/github/ederoliv/Main.java
@@ -3,29 +3,17 @@ package io.github.ederoliv;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Calculadora1");
+        System.out.println("Calculadora de Juros compostos");
 
-        double valorFinal = 0;
-        double valorInicial = 1000;
+        double valorFinal;
+        double valorInicial = 1500;
         double taxaJuros = 10;
-        double tempoInvestimento = 12;
+        double tempoInvestimento = 1;
 
 
-        valorFinal =  valorInicial + (Math.pow(1 + taxaJuros, tempoInvestimento));
+        valorFinal =  valorInicial * Math.pow(1 + taxaJuros, tempoInvestimento);
 
         System.out.println("Valor final: " + valorFinal);
 
-        /*
-
-        A = P (1 + i) ^ t
-
-    Para entender melhor a fórmula segue a explicação de cada variável:
-
-    A = valorFinal, ou seja, o resultado que você terá;
-    P = valorInicial depositado;
-    i = taxaJuros;
-    t = tempo do investimento.
-
-         */
     }
 }
