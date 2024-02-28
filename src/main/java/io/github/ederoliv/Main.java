8e1660662cdfb27b91f1b38796853a158cdda048
ederoliv <ederoliveira152@gmail.com>
2024-02-28 00:15:15 -0300

    Add gitignore

--- src/main/java/io/github/ederoliv/Main.java
+++ src/main/java/io/github/ederoliv/Main.java
@@ -1,9 +1,12 @@
 package io.github.ederoliv;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Calculadora de Juros compostos");
+        menu();
 
         double valorFinal;
         double valorInicial = 1500;
@@ -16,4 +19,34 @@ public class Main {
         System.out.println("Valor final: " + valorFinal);
 
     }
+
+    public static void menu() {
+
+        var inputUsuario = new BufferedReader(new InputStreamReader(System.in));
+
+        String letreiro = """
+                ::::::'##:::::::'##::'######:::'#######::'##::::'##:'########::
+                :::::: ##::::::: ##:'##... ##:'##.... ##: ###::'###: ##.... ##:
+                :::::: ##::::::: ##: ##:::..:: ##:::: ##: ####'####: ##:::: ##:
+                :::::: ##::::::: ##: ##::::::: ##:::: ##: ## ### ##: ########::
+                '##::: ##:'##::: ##: ##::::::: ##:::: ##: ##. #: ##: ##.....:::
+                 ##::: ##: ##::: ##: ##::: ##: ##:::: ##: ##:.:: ##: ##::::::::
+                . ######::. ######::. ######::. #######:: ##:::: ##: ##::::::::
+                :......::::......::::......::::.......:::..:::::..::..:::::::::
+                :::::::::Calculadora de Juros Compostos em Java!:::::::::::::::
+                """;
+        System.out.println(letreiro + "\n Deseja fazer o cálculo por mês(1) ou ano(20) ? \n Escolha uma opção");
+
+
+    }
+
+    public static void calculoMensal(BufferedReader input) {
+
+    }
+
+    public static void calculoAnual (BufferedReader input) {
+
+    }
+
+
 }
